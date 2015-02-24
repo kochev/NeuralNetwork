386a1fb69c07c72ea95ed94a3b9ecb1f0b3cd306
Алексей Кочев <kochev.alexey@gmail.com>
2015-02-24 21:09:16 +0500

    first commit

---
+++ NeuralNetwork/NeuralNetwork/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetwork
+{
+    class Car
+    {
+        /// <summary>
+        /// Масса
+        /// </summary>
+        public float Weight { get; set; }
+
+        /// <summary>
+        /// Грузоподъемность
+        /// </summary>
+        public float Capacity { get; set; }
+
+        /// <summary>
+        /// Привод
+        /// </summary>
+        public float Drive { get; set; }
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        public float Width { get; set; }
+
+        /// <summary>
+        /// Высота
+        /// </summary>
+        public float Height { get; set; }
+
+        /// <summary>
+        /// Длина
+        /// </summary>
+        public float Length { get; set; }
+
+        /// <summary>
+        /// Клиренс
+        /// </summary>
+        public float Clearance { get; set; }
+
+        /// <summary>
+        /// Количество лошадинных сил
+        /// </summary>
+        public float Power { get; set; }
+
+        /// <summary>
+        /// Количество пассажиров
+        /// </summary>
+        public float CountOfPassengers { get; set; }
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/DataNormalizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class DataNormalizer
+    {
+        private List<Car> Cars { get; set; }
+
+
+        public DataNormalizer(List<Car> cars)
+        {
+            Cars = cars;
+        }
+
+        public List<Car> Normalize()
+        {
+            float maxWeight = Cars.Max(obj => obj.Weight);
+            float maxCapacity = Cars.Max(obj => obj.Capacity);
+            float maxClearance = Cars.Max(obj => obj.Clearance);
+            float maxDrive = Cars.Max(obj => obj.Drive);
+            float maxWidth = Cars.Max(obj => obj.Width);
+            float maxHeight = Cars.Max(obj => obj.Height);
+            float maxLength = Cars.Max(obj => obj.Length);
+            float maxCountOfPassengers = Cars.Max(obj => obj.CountOfPassengers);
+            float maxPower = Cars.Max(obj => obj.Power);
+
+            foreach (var car in Cars)
+            {
+                car.Weight = car.Weight / maxWeight;
+                car.Capacity = car.Capacity / maxCapacity;
+                car.Clearance = car.Clearance / maxClearance;
+                car.Drive = car.Drive / maxDrive;
+                car.Width = car.Width / maxWidth;
+                car.Height = car.Height / maxHeight;
+                car.Length = car.Length / maxLength;
+                car.CountOfPassengers = car.CountOfPassengers / maxCountOfPassengers;
+                car.Power = car.Power / maxPower;
+            }
+            return Cars;
+        }
+    }
+}
